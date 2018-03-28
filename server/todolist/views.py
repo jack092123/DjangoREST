4db8bb8e636a3223f799d40b37f77b417092a534
RoachOwO <jack092123@gmail.com>
2018-03-28 20:57:28 +0800

    Request port depends on os env

--- server/todolist/views.py
+++ server/todolist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +12,12 @@ def hello_django(request):
     })
 
 def todolist_view(request):
-    todolist_data = requests.get('http://127.0.0.1:8000/api/Todo/')
+    port = os.environ.get('DJANGO_PORT')
+    if port == None:
+        port = r'8000'
+    
+    url = 'http://127.0.0.1:' + port + '/api/Todo/'
+    todolist_data = requests.get(url)
     logger.error(todolist_data.json())
     return render(request, 'todolist.html', {
         'data' : todolist_data.json()
