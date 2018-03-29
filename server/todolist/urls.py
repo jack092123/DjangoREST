55ef626569f759466450289cbcd57c8cfc3e667a
RoachOwO <jack092123@gmail.com>
2018-03-29 11:52:57 +0800

    Add edit page and function using HTTP PUT api calling

--- server/todolist/urls.py
+++ server/todolist/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.todolist_view, name="list"),
     url(r'^hello/', views.hello_django, name="hello"),
-    url(r'^edit/[0-9]/', views.edit_view, name="edit"),
+    url(r'^edit/(?P<todo_id>[0-9])/', views.edit_view, name="edit"),
 ]
--- server/todolist/views.py
+++ server/todolist/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 import requests
 import os
 
+port = os.environ.get('DJANGO_PORT')
+if port == None:
+    port = r'8000'
+
 # Create your views here.
 def hello_django(request):
     return render(request, 'hello_django.html', {
@@ -9,12 +13,15 @@ def hello_django(request):
     })
 
 def todolist_view(request):
-    port = os.environ.get('DJANGO_PORT')
-    if port == None:
-        port = r'8000'
-    
     url = 'http://127.0.0.1:' + port + '/api/Todo/'
     todolist_data = requests.get(url)
     return render(request, 'todolist.html', {
         'data' : todolist_data.json()
+    })
+
+def edit_view(request, todo_id):
+    url = 'http://127.0.0.1:' + port + '/api/Todo/' + todo_id + '/'
+    todo_data = requests.get(url)
+    return render(request, 'edit.html', {
+        'data' : todo_data.json()
     })
